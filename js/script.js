e509441b36b71448864ac48ad6801bca0cb65b4b
stephancloete <stephancloete@gmail.com>
2022-07-24 22:44:16 +0200

    Update script.js
    removed zero check, since zero is an even number

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 let input = prompt("Enter a number below to find out if it is ODD or EVEN:");
             
-            input = parseInt(input);
+input = parseInt(input);
 
-            function printOddEven(){
-               
-                if(input % 2 == 0){
-                    document.getElementById("answer").innerHTML = "Even";
-                }else{
-                    document.getElementById("answer").innerHTML = "Odd";
-                }
-            }
+function printOddEven(){
+    
+    if(input % 2 == 0){
+        document.getElementById("answer").innerHTML = "Even";
+    }else{
+        document.getElementById("answer").innerHTML = "Odd";
+    }
+}
 
-                printOddEven();
+    printOddEven();
 
-           function checkZero(){
-            if(input == 0){
-                alert("Please refresh and enter a number greater than zero");
-                document.getElementById("answer").innerHTML = "0";
-                }
-            }
+/*function checkZero(){
+if(input == 0){
+    alert("Invalid entry. Please refresh and enter a number greater than zero");
+    document.getElementById("answer").innerHTML = "0";
+    }
+}
 
-                checkZero();
+    checkZero();*/
 
-            function checkNumber(){
-        
-                if(isNaN(input)){
-                    alert("Please refresh and enter a number");
-                    document.getElementById("answer").innerHTML = "";
-                }
-            }
+function checkNumber(){
 
-            checkNumber();
+    if(isNaN(input)){
+        alert("Invalid entry. Please refresh and enter a number");
+        document.getElementById("answer").innerHTML = "";
+    }
+}
+
+checkNumber();
