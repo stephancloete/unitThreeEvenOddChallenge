83fb1fbac8dcf2627126845753463fd67624bee4
stephancloete <stephancloete@gmail.com>
2022-07-24 22:35:30 +0200

    Update script.js
    Added control statements/conditionals to ensure that the user has input a number greater than zero.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 let input = prompt("Enter a number below to find out if it is ODD or EVEN:");
             
-            input = parseInt(input); 
+            input = parseInt(input);
 
+            
+                
+           
             function printOddEven(){
                
                 if(input % 2 == 0){
@@ -9,6 +12,25 @@ let input = prompt("Enter a number below to find out if it is ODD or EVEN:");
                 }else{
                     document.getElementById("answer").innerHTML = "Odd";
                 }
-           }
+            }
 
-           printOddEven();
+                printOddEven();
+
+           function checkZero(){
+            if(input == 0){
+                alert("Please refresh and enter a number greater than zero");
+                document.getElementById("answer").innerHTML = "0";
+                }
+            }
+
+                checkZero();
+
+            function checkNumber(){
+        
+                if(isNaN(input)){
+                    alert("Please refresh and enter a number");
+                    document.getElementById("answer").innerHTML = "";
+                }
+            }
+
+            checkNumber();
